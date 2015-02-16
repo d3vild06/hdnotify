255550eddc43033fc1834a496a5496d06bcfd693
sgtquezada <sgtquezada@gmail.com>
2015-02-15 22:19:56 -0700

    app name update

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ app.listen(config.port);
 exports = module.exports = app;
 
 // Logging initialization
-console.log('MEAN.JS application started on port ' + config.port);
+console.log('HDNotify application started on port ' + config.port);
