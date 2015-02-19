884bce377cc09e401ed8940976d015593737f288
sgtquezada <sgtquezada@gmail.com>
2015-02-19 08:01:20 -0700

    added date-picker

--- config/env/all.js
+++ config/env/all.js
@@ -31,7 +31,8 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angular-datepicker/dist/index.js'
 			]
 		},
 		css: [
