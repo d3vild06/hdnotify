6ef7a02ddf58f8daf0b195aacd3f597bbfe5e970
Roberto Quezada <sgtquezada@gmail.com>
2015-05-14 16:31:12 -0400

    prod build

--- config/env/production.js
+++ config/env/production.js
@@ -5,19 +5,30 @@ module.exports = {
 	assets: {
 		lib: {
 			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/bootstrap/dist/css/bootstrap.css',
+                'public/lib/metisMenu/dist/metisMenu.min.css',
+                'public/lib/fontawesome/css/font-awesome.min.css',
+                'public/lib/angular-bootstrap-datetimepicker/src/css/datetimepicker.css'
 			],
 			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
+				'public/lib/jquery/dist/jquery.min.js',
+                'public/lib/bootstrap/dist/js/bootstrap.min.js',
+                'public/lib/metisMenu/dist/metisMenu.min.js',
+                'public/lib/PACE/pace.min.js',
+                'public/scripts.js',
+				'public/lib/angular/angular.js',
+				'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-cookies/angular-cookies.js', 
 				'public/lib/angular-animate/angular-animate.js', 
 				'public/lib/angular-touch/angular-touch.js', 
 				'public/lib/angular-sanitize/angular-sanitize.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angular-datepicker/dist/index.js',
+				'public/lib/ng-simplePagination/simplePagination.js',
+				'public/lib/moment/min/moment.min.js',
+				'public/lib/angular-bootstrap-datetimepicker/src/js/datetimepicker.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
